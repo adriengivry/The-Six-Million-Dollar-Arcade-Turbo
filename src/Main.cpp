f94636302019c53cca4c8917d685e80db25ea38d
maxbrun <m.brun@student.isartdigital.com>
2018-02-05 14:23:03 -0500

    Remove comment

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,11 +10,6 @@ using namespace video;
 using namespace io;
 using namespace gui;
 
-//#ifdef _IRR_WINDOWS_
-//#pragma comment(lib, "Irrlicht.lib")
-//#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
-//#endif
-
 int main()
 {
 	EventManager receiver;
