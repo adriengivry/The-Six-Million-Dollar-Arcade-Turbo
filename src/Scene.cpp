db4cda9f0b043e6a92204daadf7c51f326985e38
adrien <adrien@givry.pro>
2018-02-06 12:15:14 -0500

    Using irrlicht gravity and collisions (twaking values)

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -10,19 +10,19 @@ Scene::Scene(SharedContext* p_sharedContext) :
 
 void Scene::InitScene()
 {
-	auto cube = m_sceneManager.addCubeSceneNode(50);
-	cube->setPosition(irr::core::vector3df(0, -80, 0));
+	auto cube = m_sceneManager.addCubeSceneNode(200);
+	cube->setPosition(irr::core::vector3df(0, -200, 0));
 	irr::scene::ITriangleSelector* selector = nullptr;
-	selector = m_sceneManager.createOctreeTriangleSelector(cube->getMesh(), cube, 128);
+	selector = m_sceneManager.createTriangleSelectorFromBoundingBox(cube);
 	cube->setTriangleSelector(selector);
 	
 	m_camera = m_sceneManager.addCameraSceneNodeFPS(
-		nullptr, 100.0f, 0.05f, -1,
+		nullptr, 100.0f, 0.3f, -1,
 		m_sharedContext->eventManager->GetKeyMap("FPS_CAMERA"),
-		m_sharedContext->eventManager->GetKeyMapSize("FPS_CAMERA"), true);
+		m_sharedContext->eventManager->GetKeyMapSize("FPS_CAMERA"), true, 3.f);
 	irr::scene::ISceneNodeAnimator* anim = m_sceneManager.createCollisionResponseAnimator(
-		selector, m_camera, irr::core::vector3df(10, 10, 10),
-		irr::core::vector3df(0, -5, 0), irr::core::vector3df(0, 30, 0));
+		selector, m_camera, irr::core::vector3df(30, 50, 30),
+		irr::core::vector3df(0, -10, 0), irr::core::vector3df(0, 30, 0));
 	selector->drop(); // As soon as we're done with the selector, drop it.
 	m_camera->addAnimator(anim);
 	anim->drop();  // And likewise, drop the animator when we're done referring to it.
